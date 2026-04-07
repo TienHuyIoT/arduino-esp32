8e64433080d5a9db0473539e3e55c1942175e48e
TienHuyIoTLab <tienhuyiot@gmail.com>
2026-04-07 21:54:52 +0700

    fix modbus struct declare

--- tools/sdk/esp32/include/freemodbus/freemodbus/common/include/esp_modbus_master.h
+++ tools/sdk/esp32/include/freemodbus/freemodbus/common/include/esp_modbus_master.h
@@ -89,7 +89,7 @@ typedef struct {
     uint16_t            mb_size;            /*!< Size of mb parameter in registers */
     uint16_t            param_offset;       /*!< Parameter name (OFFSET in the parameter structure) */
     mb_descr_type_t     param_type;         /*!< Float, U8, U16, U32, ASCII, etc. */
-    mb_descr_size_t     param_size;         /*!< Number of bytes in the parameter. */
+    size_t              param_size;         /*!< Number of bytes in the parameter. */
     mb_parameter_opt_t  param_opts;         /*!< Parameter options used to check limits and etc. */
     mb_param_perms_t    access;             /*!< Access permissions based on mode */
 } mb_parameter_descriptor_t;
